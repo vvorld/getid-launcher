135df327fe30883d7033b53bd3d95ae9d04b13eb
Anton Rehemae <anton.rehemae@idcredit.info>
2022-01-19 18:05:10 +0200

    added invalidation s3

--- tools/publish-lib.js
+++ tools/publish-lib.js
@@ -36,4 +36,47 @@ const uploadLib = async () => {
   });
 };
 
+const invalidate = async () => {
+  envConfig.config();
+  const {
+    ACCESS_KEY_ID,
+    SECRET_ACCESS_KEY,
+    REGION,
+    BUCKET,
+    DISTRIBUTION_ID
+  } = process.env;
+
+  const config = {
+    accessKeyId: ACCESS_KEY_ID,
+    secretAccessKey: SECRET_ACCESS_KEY,
+    region: REGION,
+    Bucket: BUCKET,
+  };
+  aws.config.update(config);
+  const cloudFront = new aws.CloudFront(config);
+
+  const params = {
+    DistributionId: DISTRIBUTION_ID,
+    InvalidationBatch: {
+      CallerReference: `${Date.now()}`,
+      Paths: {
+        Quantity: 1,
+        Items: [
+          '/sdk/getid-web-sdk-launcher-v6*',
+        ]
+      }
+    }
+  };
+
+  cloudFront.createInvalidation(params, (err, res) => {
+    if (err) {
+      console.error(err);
+    }
+    if (res) {
+      console.log(res);
+    }
+  });
+};
+
 uploadLib();
+invalidate();
